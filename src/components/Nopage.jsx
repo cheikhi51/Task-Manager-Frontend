96b3d9ab079f39b9bbd6e186896d5aac302e3649
Mohamed <cheikhimohamed51@gmail.com>
2026-03-19 04:42:15 +0000

    made a small change in the nopage file

--- src/components/Nopage.jsx
+++ src/components/Nopage.jsx
@@ -3,6 +3,6 @@ function  NoPage() {
         <div className="no-page-section">
             <h2 className="no-page-title">404 - Page Not Found</h2>
         </div>
-    );
+    )
 }
 export default NoPage;
