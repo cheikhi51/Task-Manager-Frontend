e3df0b152b91fb10eaf21b6bbd029380db41ea97
Mohamed <cheikhimohamed51@gmail.com>
2026-05-07 13:04:55 +0100

    made a few changes

--- src/components/Nopage.jsx
+++ src/components/Nopage.jsx
@@ -2,6 +2,8 @@ function  Nopage() {
     return (
         <div className="no-page-section">
             <h2 className="no-page-title">404 - Page Not Found</h2>
+            <p className="no-page-message">Sorry, the page you are looking for does not exist.</p>
+                <a href="/" className="no-page-link">Go back to Home</a>
         </div>
     )
 }
